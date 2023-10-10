1899c0c0e85b3c6cbc532563655e2ed21b870b80
achucarrob <achucarroclaudia@gmail.com>
2023-10-10 16:30:20 -0300

    Order by descending price

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ url = "https://www.farmacenter.com.py/catalogo/tratamientos-dermocosmeticos-c66"
 wanted_list = ["Uriage Hyséac Gel Limpiador Purificante Gel Nettoyant - Pomo de 150 ml","₲. 113.670"]
 
 scraper = AutoScraper()
+
+# retorna una lista de resultados similares a wanted_list
 result = scraper.build(url, wanted_list)
-for i in result:
-    print(i)
-    
+
+# print(result)
+
+# Ordenar por precio de forma ascendente
+
+# agrupar/separar por nombre y precio
+result = scraper.get_result_similar(url,grouped=True)
+print(result)
