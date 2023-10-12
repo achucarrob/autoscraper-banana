9a0786ddd0e75823b996d53464549b5140186b83
achucarrob <achucarroclaudia@gmail.com>
2023-10-12 09:29:31 -0300

    order 2

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from autoscraper import AutoScraper
-
+# url de donde obtendremos los datos
 url = "https://www.farmacenter.com.py/catalogo/tratamientos-dermocosmeticos-c66"
-
+# tomamos una muestra de los datos que queremos obtener
 wanted_list = ["Uriage Hyséac Gel Limpiador Purificante Gel Nettoyant - Pomo de 150 ml","₲. 113.670"]
 
 scraper = AutoScraper()
 
 # retorna una lista de resultados similares a wanted_list
-result = scraper.build(url, wanted_list)
-
-# print(result)
+scraper.build(url, wanted_list)
 
 # Ordenar por precio de forma ascendente
 
 # agrupar/separar por nombre y precio
 result = scraper.get_result_similar(url,grouped=True)
+print(result) 
+
+# matchear cada producto con su precio
+
+scraper.set_rule_aliases({'rule_hi6e':'product', 'rule_1e4i':'price'})
+result = scraper.get_result_similar(url,group_by_alias=True)
 print(result)
+scraper.keep_rules(['rule_hi6e','rule_1e4i'])
+price = result['price']
