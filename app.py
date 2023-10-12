dcdbb6865ea570485ea403d75d7fecade7fc79d7
achucarrob <achucarroclaudia@gmail.com>
2023-10-12 15:05:43 -0300

    owner:achucarrob

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ wanted_list = ["Uriage Hyséac Gel Limpiador Purificante Gel Nettoyant - Pomo de
 
 scraper = AutoScraper()
 
-# retorna una lista de resultados similares a wanted_list
+# set the rules to scraper / retorna una lista de resultados similares a wanted_list
 scraper.build(url, wanted_list)
 
 # Ordenar por precio de forma ascendente
 
-# agrupar/separar por nombre y precio
+# agrupar/separar por nombre y precio by an unique id
 result = scraper.get_result_similar(url,grouped=True)
-print(result) 
-
+print(result)
 # matchear cada producto con su precio
 
 scraper.set_rule_aliases({'rule_hi6e':'product', 'rule_1e4i':'price'})
+scraper.keep_rules(['rule_hi6e','rule_1e4i'])
 result = scraper.get_result_similar(url,group_by_alias=True)
 print(result)
-scraper.keep_rules(['rule_hi6e','rule_1e4i'])
 price = result['price']
